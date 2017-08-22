1cae5b064cbbb7f4379058a568f658bf7a0f082d
Dracarys <laurine.sorel@free.fr>
2017-08-22 15:07:23 +0800

    [Test] Modification of test

--- Snack/tests/test_connect.py
+++ Snack/tests/test_connect.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
 
 class TestConnectLogout(TestCase):
@@ -7,36 +7,41 @@ class TestConnectLogout(TestCase):
     # Set up the database
     def setUp(self):
         self.user = User.objects.create(username='user', password='test')
+        basic = Permission.objects.get(codename='basic_account')
+        self.user.user_permissions.add(basic)
         self.user.set_password('test')
         self.user.save()
 
-    # Redirect to / if the form is not valid
-    def test_connect_view_denies_anonymous(self):
-        response = self.client.get('/', follow=True)
-        self.assertEqual(response.status_code, 200)
-        response = self.client.post('/', follow=True)
-        self.assertRedirects(response, '/')
-
-    # Redirect to /purchase/ if the connexion is a success
     def test_connect_success(self):
-        response = self.client.post('/', {'username': 'user', 'password': 'test'})
+        response = self.client.post(
+            '/',
+            {'username': 'user', 'password': 'test'}
+        )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/purchase/')
 
-    # Redirect to / if the connexion fail
     def test_connect_fail(self):
-        response = self.client.post('/', {'username': 'user', 'password': 'false'})
+        response = self.client.post(
+            '/',
+            {'username': 'user', 'password': 'false'}
+        )
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/')
 
-    # Redirect to /purchase/ if the user is already connected
-    def test_connect_redirect(self):
+    def test_wrong_information(self):
+        response = self.client.post(
+            '/',
+            {'username': '', 'password': 'aaa'}
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/')
+
+    def test_user_already_connect(self):
         self.client.force_login(self.user)
         response = self.client.get('/')
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, '/purchase/')
 
-    # Redirect to / if the user is connected
     def test_logout_redirect(self):
         self.client.force_login(self.user)
         response = self.client.get('/logout/')
--- Snack/tests/test_sale.py
+++ Snack/tests/test_sale.py
@@ -1,5 +1,8 @@
+import datetime
+
+from datetime import timedelta
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 
 from Snack.models import Profil, Type, Product, Purchase
 
@@ -9,6 +12,8 @@ class TestSale(TestCase):
     def setUp(self):
         self.user = User.objects.create(username='user', password='test')
         self.user.set_password('test')
+        treasurer = Permission.objects.get(codename='treasurer_account')
+        self.user.user_permissions.add(treasurer)
         self.user.save()
         self.profil = Profil(user=self.user)
         self.profil.save()
@@ -23,33 +28,72 @@ class TestSale(TestCase):
             number=2,
             price=1.2
         )
+        self.purchase1.save()
 
-    def test_form_valid(self):
-        response = self.client.post(
-            '/sale/?page=1&datepicker_start=11/22/2017&datepicker_end=11/22/2018',
+    def test_page_return_correct_purchase(self):
+        today = datetime.date.today() + timedelta(days=int(3))
+        start = datetime.date.today() - timedelta(days=int(6))
+        response = self.client.get(
+            '/sale/?page=1&datepicker_start=' + start.strftime('%m/%d/%Y') +
+            '&datepicker_end=' + today.strftime('%m/%d/%Y'),
         )
         self.assertEqual(response.status_code, 200)
+        self.assertIn(self.purchase1, response.context['purchases'].object_list)
 
-    def test_date1_sup_date2(self):
-        response = self.client.post(
-            '/sale/?page=1&datepicker_start=11/22/2018&datepicker_end=11/22/2017',
+    def test_date1_sup_date2_page_return_message_error(self):
+        today = datetime.date.today() + timedelta(days=int(1))
+        start = datetime.date.today() + timedelta(days=int(6))
+        response = self.client.get(
+            '/sale/?page=1&datepicker_start=' + start.strftime('%m/%d/%Y') +
+            '&datepicker_end=' + today.strftime('%m/%d/%Y'),
         )
         self.assertEqual(response.status_code, 200)
+        for message in response.context['messages']:
+            self.assertEqual('info', message.tags)
+            self.assertEqual(
+                'The start date is greater than the end date',
+                message.message
+            )
 
-    def test_form_invalid(self):
-        response = self.client.post(
+    #  def test_date_not_a_date(self):
+    #      response = self.client.get(
+    #          '/sale/?page=1&datepicker_start=08-21-2017&datepicker_end=08-22-2017'
+    #      )
+    #      self.assertEqual(response.status_code, 200)
+    #      for message in response.context['messages']:
+    #          self.assertIn('info', message.tags)
+    #          self.assertIn(
+    #              'what have you done...',
+    #              message.message
+    #          )
+
+    def test_no_date(self):
+        response = self.client.get(
             '/sale/?page=1&datepicker_start=&datepicker_end=',
         )
         self.assertEqual(response.status_code, 200)
+        self.assertIn(self.purchase1, response.context['purchases'].object_list)
 
-    def test_empty_page(self):
-        response = self.client.post(
-            '/sale/?page=2',
+    def test_no_purchase_selected(self):
+        today = datetime.date.today() + timedelta(days=int(3))
+        start = datetime.date.today() + timedelta(days=int(2))
+        response = self.client.get(
+            '/sale/?page=1&datepicker_start=' + start.strftime('%m/%d/%Y') +
+            '&datepicker_end=' + today.strftime('%m/%d/%Y'),
         )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(0, response.context['total'])
 
-    def test_page_not_integer(self):
-        response = self.client.post(
-            '/sale/?page=a',
+    def test_page_not_an_integer(self):
+        response = self.client.get(
+            '/sale/?page=a&datepicker_start=&datepicker_end=',
         )
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(1, response.context['purchases'].number)
+
+    def test_page_empty(self):
+        response = self.client.get(
+            '/sale/?page=2&datepicker_start=&datepicker_end=',
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(1, response.context['purchases'].number)
