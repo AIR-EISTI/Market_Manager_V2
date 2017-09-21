e501a042c1120f62e33c13344a6ff158e61608c2
Tjiho Lun <tom.darboux@eisti.fr>
2017-09-21 19:17:06 +0200

    [FIX]:update session time when action

--- Snack/middleware.py
+++ Snack/middleware.py
@@ -12,6 +12,13 @@ class LocaleMiddleware(MiddlewareMixin):
     def process_request(self, request):
         today = datetime.datetime.today()
         if request.user.is_authenticated:
+            print("----------------------------------------")
+            print((today - request.user.last_login).seconds)
+            print("----------------------------------------")   
             if (today - request.user.last_login).seconds > 120:
                 logout(request)
                 return HttpResponseRedirect(reverse('connect'))
+            else:
+                request.user.last_login = today
+                request.user.save()
+                print((today - request.user.last_login).seconds)
