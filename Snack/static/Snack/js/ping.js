bff8db9994bd4b6d75b6f8bb2ffcefff18fc3e25
Tjiho Lun <tom.darboux@eisti.fr>
2017-12-17 22:28:22 +0100

    add auto logout with modal

--- Snack/static/Snack/js/ping.js
+++ Snack/static/Snack/js/ping.js
@@ -9,4 +9,9 @@ function ping()
         success : function(code_html, statut){}
 
     });
+
+    clearTimeout(timeoutSession)
+    clearTimeout(timeoutLogout)
+    timeoutSession = setTimeout(session_expire,100000);
+    timeoutLogout = setTimeout(logout,120000);
 }
